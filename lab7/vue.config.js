d84794c01394af51c5edffc9e4a9487eda9525c8
Yelyzaveta Dolhova <liza.dolhova@gforces.pl>
2022-09-27 13:34:03 +0200

    Modifying passes

--- lab7/vue.config.js
+++ lab7/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/web-dev-labs/lab7" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/web-dev-labs-lab7" : "/",
 })
